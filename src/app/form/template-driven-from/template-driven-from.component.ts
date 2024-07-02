892c5430c008af16dc4165ab6e616a31bcc8110f
mishrsmfx324 <mishrasuryansh20@yahoo.com>
2024-07-02 16:57:21 +0530

    complete template driven form

--- src/app/form/template-driven-from/template-driven-from.component.ts
+++ src/app/form/template-driven-from/template-driven-from.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { FormsModule, NgForm } from "@angular/forms";
 
 @Component({
@@ -11,10 +11,53 @@ import { FormsModule, NgForm } from "@angular/forms";
 })
 export class TemplateDrivenFromComponent {
 
+  RESET_FORM = 0;
+  SET_FORM = 1;
+  PATCH_FORM = 2;
+
+  @ViewChild('f')
+  userForm!: NgForm;
+
+  preferredLocations = [{
+    name: 'Dehradun',
+    code: 101
+  }, {
+    name: 'Rishikesh',
+    code: 102
+  }, {
+    name: 'Nainital',
+    code: 103
+  }];
+
   onSubmit(form: NgForm) {
     console.log(form);
     console.log(form.controls);
     console.log(form.value);
     console.log("form is submitted");
+  }
+
+  set(task: number) {
+    switch(task) {
+      case this.SET_FORM:
+        this.userForm.setValue({
+          "basicDetails": {
+              "username": "money is the one",
+              "email": "money@istheone.com"
+          },
+          "secret": "Mother's Name?",
+          "preferredLocation": this.preferredLocations[1]
+      });
+        break;
+      case this.RESET_FORM:
+        this.userForm.reset();
+        break;
+      case this.PATCH_FORM:
+        this.userForm.form.patchValue({
+          "secret": "First Pet's Name?",
+          "preferredLocation": this.preferredLocations[2]
+        });
+        break;
+      default:
+    }
   }
 }
